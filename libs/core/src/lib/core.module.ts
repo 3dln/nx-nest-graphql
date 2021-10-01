a31514dcda16f44ff0768360ff83617f9e02bded
Ashkan Ashtiani <ashcan@3dln.com>
2021-10-01 21:11:30 +0330

    nestjs config for env and joi for validation and to use in core library

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -1,6 +1,16 @@
+import { validationSchema } from './config/validation';
+import { configuration } from './config/configuration';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
   exports: [],
